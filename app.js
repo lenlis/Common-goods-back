289898cfc47848e2940d9a3ec7dd9827b8483e01
lenlis <sit.a.v@mail.ru>
2025-01-11 03:41:40 +0500

    тест корс

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const errorMiddleware = require('./middlewares/error-middleware');
 
 
 var app = express();
-app.use(cors({
-  // credentials: true,
-  // origin: process.env.CLIENT_URL
-}));
+// app.use(cors({
+//   credentials: true,
+//   origin: process.env.CLIENT_URL
+// }));
+app.use(cors({origin: '*'}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
