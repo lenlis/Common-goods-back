0724caf82b93c7926531707831386c7334a31ef0
lenlis <sit.a.v@mail.ru>
2025-01-11 17:32:01 +0500

    дебаг

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -4,22 +4,26 @@ module.exports = function (req, res, next) {
     try {
         const authorizationHeader = req.headers.authorization;
         if (!authorizationHeader) {
+            console.log("Заголовок")
             return next(ApiError.UnauthorizedError());
         }
 
         const accessToken = authorizationHeader.split(' ')[1];
         if (!accessToken) {
+            console.log("Токен")
             return next(ApiError.UnauthorizedError());
         }
 
         const userData = TokenService.validateAccessToken(accessToken);
         if (!userData) {
+            console.log("Дата")
             return next(ApiError.UnauthorizedError());
         }
 
         req.user = userData;
         next();
     } catch (e) {
+        console.log(e)
         return next(ApiError.UnauthorizedError());
     }
 };
