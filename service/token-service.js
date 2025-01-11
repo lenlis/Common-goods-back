045c589d157e9eeb0bbf38d88faef55dc806a785
lenlis <sit.a.v@mail.ru>
2025-01-11 18:18:10 +0500

    увеличение срока службы токена

--- service/token-service.js
+++ service/token-service.js
@@ -3,7 +3,7 @@ const { prisma } = require("../prisma/prisma-client");
 
 class TokenService {
     static generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '100s'});
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'});
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'});
         return {
             accessToken,
